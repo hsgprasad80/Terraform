2681b4c16b3624c157909c9eac60ba943c818c06
Guru Prasad <hsgprasad@gmail.com>
2021-02-27 12:27:45 +0000

    ami updated to linux

--- Chef-workstation/main.tf
+++ Chef-workstation/main.tf
@@ -15,8 +15,8 @@ provider "aws" {
 #create instance
 resource "aws_instance" "Chef-workstation" {
   count           = var.number
- #ami             = var.redhatami
-  ami 		        = var.ubuntuami	
+  ami             = var.redhatami
+ # ami 		        = var.ubuntuami	
   instance_type   = var.instance_type
   security_groups = ["sg-0b960783f63a19067"]
   subnet_id       = "subnet-03b5034a84bdcec1f"
@@ -24,8 +24,8 @@ resource "aws_instance" "Chef-workstation" {
   user_data       = <<-EOF
       #!/bin/bash
         sudo yum update -y
-		sudo wget https://packages.chef.io/files/stable/chef-workstation/20.7.96/el/7/chef-workstation-20.7.96-1.el7.x86_64.rpm
-		sudo yum install chef-workstation-20.7.96-1.el7.x86_64.rpm -y
+		#sudo wget https://packages.chef.io/files/stable/chef-workstation/20.7.96/el/7/chef-workstation-20.7.96-1.el7.x86_64.rpm
+		#sudo yum install chef-workstation-20.7.96-1.el7.x86_64.rpm -y
         sudo yum install tree -y
         sudo yum install httpd -y
         sudo service httpd start 
