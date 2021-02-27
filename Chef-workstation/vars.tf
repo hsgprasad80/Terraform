ed027a6c7f8b5630259b42a6576b28c131b7b2a9
Guru Prasad <hsgprasad@gmail.com>
2021-02-27 11:54:03 +0000

    ami updated

--- Chef-workstation/vars.tf
+++ Chef-workstation/vars.tf
@@ -8,7 +8,7 @@ variable "ubuntuami" {
   default = "ami-03d315ad33b9d49c4"
 }
 variable "redhatami" {
-  default = "ami-047a51fa27710816e"
+  default = "ami-0915bcb5fa77e4892"
 }
 variable "Privkey_name" {
   default = "private-key"
